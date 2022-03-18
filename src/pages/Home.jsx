495a0d6b7c4833c45f3a76844099ad184b1a0503
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-03-18 18:27:55 +0700

    004. add navigation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,22 +2,26 @@ import React from 'react'
 
 import {FaFile} from 'react-icons/fa';
 import {MdGpsFixed} from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
       <div className='container mx-auto px-4 py-6'>
       
       <div className='w-full flex gap-2'>
+
       <div className="w-1/2 justify-center text-center bg-blue-300 rounded-lg">
-    
+    <Link to="/upload">
       <h1 className='text-2xl mt-8'>Upload File</h1>
       <FaFile className='w-1/5 h-64 mx-auto'/>
+      </Link>
      
       </div>
        <div className="w-1/2 justify-center text-center bg-blue-300 rounded-lg">
-    
+    <Link to="/gps">
       <h1 className='text-2xl mt-8'>GPS</h1>
       <MdGpsFixed className='w-1/5 h-64 mx-auto'/>
+      </Link>
      
       </div>
       </div>
--- src/utils/NavigatorBottom/index.jsx
+++ src/utils/NavigatorBottom/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaHome,FaFile } from "react-icons/fa";
 import {MdGpsFixed} from 'react-icons/md';
+import {Link} from 'react-router-dom';
 
 const NavigatorBottom = () => {
   return (
@@ -9,18 +10,24 @@ const NavigatorBottom = () => {
         {/* <section id="bottom-navigation" class="md:hidden block fixed inset-x-0 bottom-0 z-10 bg-white shadow"> // if shown only tablet/mobile*/}
         <section id="bottom-navigation" className="block fixed inset-x-0 bottom-0 z-10 bg-blue-300 shadow">
           <div id="tabs" className="flex justify-between">
+          <Link to="/">
             <div  className=" w-full text-center inline-block align-middle px-2 py-2">
              <FaHome className='mx-auto w-36 h-9'/>
               <h1 className="tab tab-home block text-xl">Home</h1>
             </div>
+            </Link>
+            <Link to="/gps">
            <div  className=" w-full text-center inline-block align-middle px-2 py-2">
              <MdGpsFixed className='mx-auto w-36 h-9'/>
               <h1 className="tab tab-home block text-xl">GPS</h1>
             </div>
+            </Link>
+            <Link to="/upload">
            <div  className=" w-full text-center inline-block align-middle px-2 py-2">
              <FaFile className='mx-auto w-36 h-9'/>
               <h1 className="tab tab-home block text-xl">Files</h1>
             </div>
+            </Link>
          
       
           </div>
