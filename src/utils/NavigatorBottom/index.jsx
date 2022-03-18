146cd90acc4277e9158cb72a7fb99afb7edede4b
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-03-18 22:19:22 +0700

    012. update bottom nav

--- src/utils/NavigatorBottom/index.jsx
+++ src/utils/NavigatorBottom/index.jsx
@@ -12,19 +12,19 @@ const NavigatorBottom = () => {
           <div id="tabs" className="flex justify-between">
           <Link to="/">
             <div  className=" w-full text-center inline-block align-middle px-2 py-2">
-             <FaHome className='mx-auto w-36 h-9'/>
+             <FaHome className='mx-auto w-20 h-7'/>
               <h1 className="tab tab-home block text-xl">Home</h1>
             </div>
             </Link>
             <Link to="/gps">
            <div  className=" w-full text-center inline-block align-middle px-2 py-2">
-             <MdGpsFixed className='mx-auto w-36 h-9'/>
+             <MdGpsFixed className='mx-auto w-20 h-7'/>
               <h1 className="tab tab-home block text-xl">GPS</h1>
             </div>
             </Link>
             <Link to="/upload">
            <div  className=" w-full text-center inline-block align-middle px-2 py-2">
-             <FaFile className='mx-auto w-36 h-9'/>
+             <FaFile className='mx-auto w-20 h-7'/>
               <h1 className="tab tab-home block text-xl">Files</h1>
             </div>
             </Link>
